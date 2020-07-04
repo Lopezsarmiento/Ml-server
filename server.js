f3d08853674d2b59b890ed399d32f1c03d1c993e
lopezsarmiento <diego.lopezsarmiento@gmail.com>
2020-07-04 02:12:54 -0300

    adding middleware

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+const debug = require("debug")("app:startup");
 const express = require("express");
 const axios = require("axios");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
 const app = express();
+
+// displays process.env.NODE_ENV
+// if not set it returns development
+console.log(`app: ${app.get("env")}`);
+
+// middleware functions
+// You can use app.use() to specify middleware as the callback function
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  debug("Morgan enabled for development");
+}
 
 const baseUrl = "https://api.mercadolibre.com/";
 const limit = "&&limit=2";
